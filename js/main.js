2bc9c936aa5ea9ed34b9a7f174aed2ab9d20163c
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-11-15 11:21:33 -0600

    added bootstrap and font. beginning styling

--- js/main.js
+++ js/main.js
@@ -18,10 +18,10 @@ var coffees = [
 ];
 
 function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
+    var html = '<div class="row col-6 coffee">';
     html += '<div class="remove-id">' + coffee.id + '</div>';
-    html += '<h3>' + coffee.name + '</h3>';
-    html += '<p>' + coffee.roast + '</p>';
+    html += '<h3 class="col-3">' + coffee.name + '</h3>';
+    html += '<p class="col-3">' + coffee.roast + '</p>';
     html += '</div>';
     return html;
 
