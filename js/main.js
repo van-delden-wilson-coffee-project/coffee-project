3fe8fe8b8a5b3c1840a3dea7eb20e776c088ae7f
Luke Van Delden <vandelden.luke@gmail.com>
2022-11-15 10:46:04 -0600

    clarifying comments

--- js/main.js
+++ js/main.js
@@ -1,70 +1,5 @@
 "use strict";
 
-
-function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    html += '<div class="remove-id">' + coffee.id + '</div>';
-    html += '<h3>' + coffee.name + '</h3>';
-    html += '<p>' + coffee.roast + '</p>';
-    html += '</div>';
-    return html;
-}
-
-function renderCoffees(coffees) {
-    var html = '';
-    for(var i = 0; i < coffees.length; i++) {
-        html += renderCoffee(coffees[i]);
-    }
-    return html;
-}
-// DISPLAYS ALL COFFEES
-
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function (coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        } else if (selectedRoast === 'all') {
-            filteredCoffees.push(coffee);
-        }
-    });
-    dbody.innerHTML = renderCoffees(filteredCoffees);
-}
-
-function updateSearch() {
-    let compare = coffeeToSearch.value;
-    let matching = [];
-    coffees.forEach(function (coffee) {
-        if (coffee.name.toLowerCase().indexOf(compare.toLowerCase()) !== -1 || coffee.roast.toLowerCase().indexOf(compare) !== -1) {
-            matching.push(coffee);
-        }
-    });
-    dbody.innerHTML = renderCoffees(matching);
-}
-// TODO: create a new object for the array of coffees that is added to coffees
-function addCoffee(e) {
-    e.preventDefault();
-    let coffeeObj = {
-        id: coffees.length + 1,
-        name: submitText.value,
-        roast: roastAdd.value
-    };
-    coffees.push(coffeeObj);
-
-    dbody.innerHTML = renderCoffees(coffees);
-
-    // Set Item
-    localStorage.setItem(coffeeStorage, JSON.stringify(coffeeObj));
-// Retrieve
-    JSON.parse(window.localStorage.getItem(coffeeStorage));
-    console.log(JSON.parse(window.localStorage.getItem(coffeeStorage)));
-
-    // document.getElementById("coffees").innerHTML = localStorage.getItem("Mr.Coffee");
-}
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
@@ -82,20 +17,84 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
+function renderCoffee(coffee) {
+    var html = '<div class="coffee">';
+    html += '<div class="remove-id">' + coffee.id + '</div>';
+    html += '<h3>' + coffee.name + '</h3>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
+    return html;
+
+}
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = 0; i < coffees.length; i++) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+// DISPLAYS ALL COFFEES
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var filteredCoffees = [];
+    coffees.forEach(function (coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        } else if (selectedRoast === 'all') {
+            filteredCoffees.push(coffee);
+        }
+    });
+    dbody.innerHTML = renderCoffees(filteredCoffees);
+
+}
+//Updates search on keyup
+function updateSearch() {
+    let compare = coffeeToSearch.value;
+    let matching = [];
+    coffees.forEach(function (coffee) {
+        if (coffee.name.toLowerCase().indexOf(compare.toLowerCase()) !== -1 || coffee.roast.toLowerCase().indexOf(compare) !== -1) {
+            matching.push(coffee);
+        }
+    });
+    dbody.innerHTML = renderCoffees(matching);
+}
+// TODO: create a new object for the array of coffees that is added to coffees
+
+var coffeeStorage = coffees.length + 1
+if (localStorage.length > 0) {
+    coffees.push(JSON.parse(window.localStorage.getItem(coffeeStorage)));
+}
+
+// CREATES COFFEE OBJ AND ADDS TO COFFEES ARRAY
+function addCoffee(e) {
+    e.preventDefault();
+
+    let coffeeObj = {
+        id: coffees.length + 1,
+        name: submitText.value,
+        roast: roastAdd.value
+    };
+
+    coffees.push(coffeeObj);
+    dbody.innerHTML = renderCoffees(coffees);
+    localStorage.setItem(coffeeStorage, JSON.stringify(coffeeObj));
+    JSON.parse(window.localStorage.getItem(coffeeStorage));
+
+    // TODO:Use below for testing localstorage
+    // console.log(JSON.parse(window.localStorage.getItem(coffeeStorage)));
+}
 
 
-const input = document.querySelector('input');
+
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var dbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit-coffee');
 var roastSelection = document.querySelector('#roast-selection');
 var roastAdd = document.getElementById("added-coffee")
 var coffeeToSearch = document.getElementById("coffee-search");
 var submitText = document.getElementById('submit-text');
-var coffeeStorage = coffees.length + 1
-
-if (localStorage.length > 0) {
-    coffees.push(JSON.parse(window.localStorage.getItem(coffeeStorage)));
-}
 
 
 dbody.innerHTML = renderCoffees(coffees);
