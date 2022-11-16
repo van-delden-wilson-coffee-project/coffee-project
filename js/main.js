a2ecf4cd023a3f7f722cece975edf99f91932076
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-11-16 14:03:25 -0600

    added comments and rearranged css class

--- js/main.js
+++ js/main.js
@@ -29,21 +29,23 @@ function renderCoffee(coffee) {
 // DISPLAYS ALL COFFEES
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = 0; i < coffees.length; i++) {
+    for(let i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 //function for search and roast input fields
+//CONDITION 1: if elements of coffee name are equal to index of searched value AND coffee.roast is equal to the selected roast in dropdown
+//CONDITION 2: if elements of coffee name OR coffee roast are equal to index of searched value AND dropdown is equal to all OR an empty value
 function updateSearch() {
-    let compare = coffeeToSearch.value; //takes the value of coffee-search input field
-    var selectedRoast = roastSelection.value;
-    let matching = [];
+    let compare = coffeeToSearch.value; //gets value of coffee-search input field
+    let selectedRoast = roastSelection.value; //gets value of dropdown
+    let matching = []; //new array of coffees
         coffees.forEach(function (coffee) {
             if (coffee.name.toLowerCase().indexOf(compare.toLowerCase()) !== -1 && coffee.roast === selectedRoast) {
-                matching.push(coffee);
+                matching.push(coffee); //push coffee to the array of coffees
             } else if ((coffee.name.toLowerCase().indexOf(compare.toLowerCase()) !== -1 || coffee.roast.toLowerCase().indexOf(compare) !== -1) && selectedRoast === 'all' || selectedRoast === '') {
-                matching.push(coffee);
+                matching.push(coffee); //push coffee to new array
             }
         });
     dbody.innerHTML = renderCoffees(matching);
@@ -65,7 +67,7 @@ function addCoffee(e) {
     };
     coffees.push(coffeeObj);
     dbody.innerHTML = renderCoffees(coffees);
-    localStorage.setItem(coffeeObj.id.toString(), JSON.stringify(coffeeObj));
+    localStorage.setItem(coffeeObj.id.toString(), JSON.stringify(coffeeObj)); //key(id): "coffeeObj"
     JSON.parse(window.localStorage.getItem(coffeeObj.id.toString()));
     submitText.value = ''; //resets typed value
 }
